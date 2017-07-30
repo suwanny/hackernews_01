8c3755047deef9562301b0ba3ab7149705fc28f9
Soo Hwan Park <sopark@linkedin.com>
2017-07-30 12:52:32 -0700

    Chap1: 01

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React, {Component} from 'react';
+
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
+    const header = "Welcome to the Road to learn React 4";
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>{header}</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+import './index.css';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(< App / >, document.getElementById('root'));
 registerServiceWorker();
+
+if (module.hot) {
+  module
+    .hot
+    .accept();
+}

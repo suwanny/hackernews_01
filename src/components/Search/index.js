237149b71b0e01534cf84ee6de068205be4ca8ab
Soo Hwan Park <sopark@linkedin.com>
2017-08-20 13:02:16 -0700

    Chap 5: Ref a DOM element

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,11 +1,34 @@
 // functional stateless component
-import React from 'react';
+import React, {Component}  from 'react';
 
-const Search = ({value, onChange, onSubmit, children}) => <form onSubmit={onSubmit}>
-  <input type="text" value={value} onChange={onChange}/>
-  <button type="submit">
-    {children}
-  </button>
-</form>
+// const Search = ({value, onChange, onSubmit, children}) => <form onSubmit={onSubmit}>
+class Search extends Component {
+  componentDidMount() {
+    this.input.focus();
+  }
+
+  render() {
+    const {
+      value,
+      onChange,
+      onSubmit,
+      children
+    } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={(node) => { this.input = node; }}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    );
+  }
+}
 
 export default Search;
